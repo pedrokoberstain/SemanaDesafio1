f0d18991016a4a83ea1115413fb13e421e7b44fb
Kober <pedrokoberstain2@gmail.com>
2023-12-28 12:21:08 -0300

    Implementada a classe Funcionarios e criado a lista para armazenar os funcionarios

---
+++ Desafios/Bonus/src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import entities.Funcionarios;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos funcionários seram cadastrados ? ");
+        int quanFunci = sc.nextInt();
+
+        List<Funcionarios> funcionarios = new ArrayList<>();
+
+    }
+}
---
+++ Desafios/Bonus/src/entities/Funcionarios.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class Funcionarios {
+
+    private String nome;
+    private String salario;
+
+    public Funcionarios(String nome, String salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+}
