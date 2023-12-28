cd50b6406737d90fe295cf2217686a883b003a83
Kober <pedrokoberstain2@gmail.com>
2023-12-28 10:47:04 -0300

    Implementando a estrutura de dados Map e separando as classes

---
+++ Desafios/Login/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import entities.Usuarios;
+import times.Horarios;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Map<String, Usuarios> usuarios = new HashMap<>();
+        usuarios.put("pedro", new Usuarios("senha123"));
+        usuarios.put("henrique", new Usuarios("senha456"));
+        usuarios.put("kober", new Usuarios("senha789"));
+
+        System.out.println("Usuário: ");
+        String usuariodig = sc.nextLine();
+        System.out.println("Senha: ");
+        String senhadig = sc.nextLine();
+
+        sc.close();
+    }
+}
---
+++ Desafios/Login/src/entities/Mensagens.java
@@ -0,0 +1,11 @@
+package entities;
+
+public class Mensagens {
+
+    public static final String [] Msg = {
+            "Bom dia, você se logou ao nosso sistema.",
+            "Boa tarde, você se logou ao nosso sistema.",
+            "Boa noite, você se logou ao nosso sistema.",
+            "Boa madrugada, você se logou ao nosso sistema."
+    };
+}
